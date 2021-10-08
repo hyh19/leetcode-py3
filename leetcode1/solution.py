e5ad950e06e056085a759ef4f2bdf3a62eafc14b
huangyh <mrhuangyuhui@gmail.com>
2021-10-08 18:18:19 +0800

    leetcode1

--- leetcode1/solution.py
+++ leetcode1/solution.py
@@ -4,12 +4,11 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        n = len(nums)
-        for i in range(n):
-            for j in range(i + 1, n):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
-
+        hashtable = dict()
+        for i, num in enumerate(nums):
+            if target - num in hashtable:
+                return [hashtable[target - num], i]
+            hashtable[nums[i]] = i
         return []
 
 
