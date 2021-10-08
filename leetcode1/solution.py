26780e9912cb4395d1d678186341a5a17dc04d0b
huangyh <mrhuangyuhui@gmail.com>
2021-10-08 18:10:06 +0800

    新建仓库

---
+++ leetcode1/solution.py
@@ -0,0 +1,50 @@
+import json
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        n = len(nums)
+        for i in range(n):
+            for j in range(i + 1, n):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
+        return []
+
+
+def stringToIntegerList(input):
+    return json.loads(input)
+
+
+def integerListToString(nums, len_of_list=None):
+    if not len_of_list:
+        len_of_list = len(nums)
+    return json.dumps(nums[:len_of_list])
+
+
+def main():
+    import sys
+    import io
+    def readlines():
+        for line in io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8'):
+            yield line.strip('\n')
+
+    lines = readlines()
+    while True:
+        try:
+            line = next(lines)
+            nums = stringToIntegerList(line);
+            line = next(lines)
+            target = int(line);
+
+            ret = Solution().twoSum(nums, target)
+
+            out = integerListToString(ret);
+            print(out)
+        except StopIteration:
+            break
+
+
+if __name__ == '__main__':
+    main()
